94f3285ce1005e3dfaf29745e392f8add9a7d921
Grzegorz Brzęczyszczykiewicz <sam@brj.pp.ru>
2021-11-08 22:27:16 +0300

    2021-11-08-222716 debug line test

--- commit.sh
+++ commit.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-# git push ironman repo to github
+# git push linux-mint-brjed to github.lo
 
 dated=`date +%Y-%m-%d-%H%M%S`
 
 git add .
-echo run git push on ${dated}
 
+echo run git push on ${dated}
 
 for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 
-# git add -u .
-#
-# git commit -m 'some fix on '${dated} # nice ;-)
-
 git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
-git push -u origin master
+
+git push -u origin main
+
+#
+# non master, just a main. fck2020 (c) scooter
+#
