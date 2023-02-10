f63d4ca8f239aa6100cf7645e90a1b4cd7e1a7ca
root <sam@brj.pp.ru>
2023-02-11 02:15:08 +0700

    2023-02-11-021506 Herp derp I left the debug in there and forgot to reset errors.

--- commit.sh
+++ commit.sh
@@ -7,25 +7,13 @@
 dated=$(date +%Y-%m-%d-%H%M%S)
 commt=$(curl -L --proxy http://192.168.0.11:3129 -s http://whatthecommit.com/index.txt)
 
-echo "$dated"
-echo "$commt"
-
-
-
 git add .
 
-echo run git push on ${dated}
+echo run git push on ${dated} ${commt}
 
-for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
+git rm `git status | grep deleted | awk '{print $2}'`
 
 git commit -m "$dated $commt"
-
-exit 0
-
-curl -L http://192.168.0.11:3129 http://whatthecommit.com/index.txt
-# | grep "<p>" | sed 's/<p>//' | sed 's/^/"/' | sed 's/$/"/' | xargs git commit -m
-
-curl -L --proxy http://192.168.0.11:3129 -s http://whatthecommit.com/index.txt
 
 git push -u origin main
 
