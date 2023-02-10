64a3691ff5946dd18621f75fad34cc204eb01322
root <root@m21.brj.pp.ru>
2023-02-11 02:08:33 +0700

    2023-02-11-020830 small is a real HTML tag, who knew.

--- commit.sh
+++ commit.sh
@@ -4,7 +4,10 @@
 # git push scriptbox repo to githublo
 #
 
-dated=`date +%Y-%m-%d-%H%M%S`
+dated=$(date +%Y-%m-%d-%H%M%S)
+commt=$(curl -L --proxy http://192.168.0.11:3129 -s http://whatthecommit.com/index.txt)
+
+echo $dated
 
 git add .
 
@@ -12,8 +15,14 @@ echo run git push on ${dated}
 
 for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 
-# hardcode proxy, becouse banned in russia :-(
-git commit -m "$dated $(curl -L http://192.168.0.11:3129 -s http://whatthecommit.com/index.txt)"
+git commit -m "$dated $commt"
+
+exit 0
+
+curl -L http://192.168.0.11:3129 http://whatthecommit.com/index.txt
+# | grep "<p>" | sed 's/<p>//' | sed 's/^/"/' | sed 's/$/"/' | xargs git commit -m
+
+curl -L --proxy http://192.168.0.11:3129 -s http://whatthecommit.com/index.txt
 
 git push -u origin main
 
