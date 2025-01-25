772438f942fff63acd27095fa9dd88464ec797bb
Roman Y. Bogdanov <sam@brj.pp.ru>
2025-01-25 23:05:43 +0500

    2025-01-25-230542 [no message]

--- dotfiles/macos/bin/synpass.sh
+++ dotfiles/macos/bin/synpass.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -v /Users/brjppru/brjvault/*.kdbx "/Users/brjppru/Library/Mobile Documents/com~apple~CloudDocs/zalupo"
+cp -v /Users/brjed/brjvault/*.kdbx "/Users/brjed/Library/Mobile Documents/com~apple~CloudDocs/zalupo"
 
