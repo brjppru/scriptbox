00a7aca49cc75177e15ce522faf59ec18f26a462
Roman Bogdanov <sam@brj.pp.ru>
2025-10-25 00:26:31 +0500

    2025-10-25-002628 IEize

---
+++ dotfiles/macos/zverdvd/init.brew.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if ! command -v brew &>/dev/null; then
+    echo "📦 install homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "✅ Homebrew already installed"
+fi
+
+brew update && brew upgrade
