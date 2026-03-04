b466844af8cd474da50c871faf0e11544bbe73a7
Roman Bogdanov <sam@brj.pp.ru>
2026-03-05 00:14:50 +0500

    2026-03-05-001448 epic

---
+++ dotfiles/android/debloat-standard.sh
@@ -0,0 +1,36 @@
+service call package 136 s16 com.hihonor.id i32 0 i32 0
+
+# Everything below was generated
+pm disable-user --user 0 com.facebook.appmanager
+pm disable-user --user 0 com.facebook.services
+pm disable-user --user 0 com.facebook.system
+pm disable-user --user 0 com.google.android.gms.location.history
+pm disable-user --user 0 com.hihonor.aipluginengine
+pm disable-user --user 0 com.hihonor.airlink
+pm disable-user --user 0 com.hihonor.android.instantshare
+pm disable-user --user 0 com.hihonor.android.mirrorshare
+pm disable-user --user 0 com.hihonor.android.pushagent
+pm disable-user --user 0 com.hihonor.appmarket
+pm disable-user --user 0 com.hihonor.assistant
+pm disable-user --user 0 com.hihonor.brain
+pm disable-user --user 0 com.hihonor.contacts.sync
+pm disable-user --user 0 com.hihonor.crossdeviceserviceshare
+pm disable-user --user 0 com.hihonor.handoff
+pm disable-user --user 0 com.hihonor.hisight
+pm uninstall com.hihonor.hncloud
+pm disable com.hihonor.id
+pm uninstall com.hihonor.koBackup
+pm disable-user --user 0 com.hihonor.magicvoice
+pm disable-user --user 0 com.hihonor.mediamaterial
+pm disable-user --user 0 com.hihonor.phoneservice
+pm uninstall com.hihonor.popularapps
+pm disable-user --user 0 com.google.android.projection.gearhead
+pm disable-user --user 0 com.hihonor.android.thememanager
+pm disable-user --user 0 com.hihonor.game.kitserver
+pm uninstall com.hihonor.magichome
+pm uninstall com.block.juggle
+pm uninstall com.booking
+pm uninstall com.facebook.katana
+pm uninstall ru.vk.store
+pm uninstall com.zhiliaoapp.musically
+pm uninstall cn.wps.moffice_eng
